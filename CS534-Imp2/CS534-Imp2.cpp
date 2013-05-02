594df0ec485c96035a71f03a90b5456984d0a5b4
mendesm@onid.orst.edu <mendesm@onid.orst.edu>
2013-05-01 20:29:24 -0700

    Removed errors and warnings.
    Now getting a "debug assertion failed" thing about vector length.  I'll debug it when I wake up, unless you have time now.  Probably I mistyped a vector field somewhere.

--- CS534-Imp2/CS534-Imp2.cpp
+++ CS534-Imp2/CS534-Imp2.cpp
@@ -9,6 +9,194 @@
 
 using namespace std;
 
+// result of training: list of p_i|y and x_i for all i elements of vocabulary
+// p_i|y needs Laplace smoothing, aka add 1 to numerator and 2 to denominator
+// then to test: calculate p(x|y), straightforward enough
+/*
+	to calculate a single p(x|y)
+		p = 0
+		for all words i
+			n1 = num(class y examples where word i appears) + 1
+			n = num(class y examples) + 2
+			//p_i|y = n1/n
+			// sum of log(a)s rather than product of as
+			if i present in x
+				p += log(n1/n)
+			else
+				p += log(1 - n1/n)
+
+	so to train all
+	vector of y vectors of length i holding p_i|y
+	use above method but instead of p +=, just assign p_vec[y][i] = p_i|y
+	
+*/
+vector<pair<vector<int>, int>> naiveBayesBernoulliTraining(vector<string> labels, vector<string> dictionary, vector<pair<int, vector<pair<int, int>>>> trainingData){
+	vector<int> appearances(dictionary.size(), 1); // laplace smoothing - 1 in numerator
+	vector<pair<vector<int>, int>> p_iy(labels.size(), make_pair(appearances, 2)); // laplace smoothing - 2 in denominator
+
+	for(auto example : trainingData) {
+		int y = example.first;
+		p_iy[y].second++; // instance of class y seen
+		for(auto word : example.second) {
+			p_iy[y].first[word.first]++; // at least one appearance of word i in class y
+		}
+	}
+	return p_iy;
+}
+
+// needs test data as input -- make output
+vector<int> naiveBayesBernoulliTest(vector<pair<int, vector<pair<int, int>>>> testData, vector<pair<vector<int>, int>> p_iy) {
+	// iterative over test data
+	/*
+		for example x
+		py = 0f
+		for each possible class y
+			for each feature xi
+				if xi == 1
+					py += log( p_iy[y].first[i] / p_iy[y].second )
+				else
+					py += log ( 1 - above )
+		pick highest probability class
+	*/
+
+	vector<int> classification;			// compare to test label at some point
+	bool inSet;
+	int selection = 0;			
+
+	// for test example i
+	for(size_t i = 1; i < testData.size(); i++){
+	vector<double> py;					// remake py for each loop
+	py.push_back(0);					// pad with a 0 -- REMOVE THIS IF CLASS LABEL STARTS AT ZERO
+
+		// for each class j - SHOULD THIS START WITH 0 OR 1?
+		for(size_t j = 1; j < p_iy.size(); j++){
+			py.push_back(0);
+			//for each feature k
+			for(size_t k = 0; k < p_iy[i].first.size(); k++){
+				inSet = false;
+
+				// check if feature present
+				for(size_t m = 0; m < testData[i].second.size(); m++){
+					if(testData[i].second[m].first == k){
+						inSet = true;
+						break;
+					}
+				}
+
+				// if feature present, add log(p).  Else, add log(1 - p)
+				if(inSet == true)
+					py[j] += log(p_iy[j].first[k] / p_iy[j].second);
+				else
+					py[j] += 1 - log(p_iy[j].first[k] / p_iy[j].second);
+
+			}
+		}
+
+		// classify
+		double maxpy = -10000000000000;	// maybe replace value for initial maxpy with a constant?  
+		for(size_t j = 1; j < p_iy.size(); j++){ 
+			if(py[j] > maxpy){
+				maxpy = py[j];
+				selection = j;
+			}
+		}
+		// add to classification vector
+		classification[i] = selection;
+	}
+
+	return classification;
+
+}
+
+vector<vector<int>> formSolutionMatrix(vector<int> classification, 	vector<pair<int, vector<pair<int, int>>>> testData, int numClasses){
+	
+	// create solution matrix
+	vector<vector<int>> solution;
+	solution.resize(numClasses);			// numClasses may actually be number of classes plus one w/ zero unused
+	for(size_t i = 0; i < solution.size(); i++){
+		solution[i].resize(numClasses, 0);
+	}
+
+	for(size_t i = 0; i < classification.size(); i++){
+		solution[testData[i].first][classification[i]]++;
+	}
+
+	return solution;
+}
+
+vector<pair<vector<int>, int>> naiveBayesMultinomialTraining(vector<string> labels, vector<string> dictionary, vector<pair<int, vector<pair<int, int>>>> trainingData){
+	vector<int> appearances(dictionary.size(), 1); // laplace smoothing - 1 in numerator
+	vector<pair<vector<int>, int>> p_iy(labels.size(), make_pair(appearances, dictionary.size())); // laplace smoothing - vocab size in denominator
+
+	for(auto example : trainingData) {
+		int y = example.first;
+		for(auto word : example.second) {
+			// count all words in group/class y
+			p_iy[y].second += word.second;
+			// count of word i in class y examples
+			p_iy[y].first[word.first] += word.second;
+		}
+	}
+	return p_iy;
+}
+
+vector<int> naiveBayesMultinomialTest(vector<pair<int, vector<pair<int, int>>>> testData, vector<pair<vector<int>, int>> p_iy) {
+	// this algorithm might be exactly the same as for the Bernoulli test. we may not need both functions.
+		vector<int> classification;			// compare to test label at some point
+	bool inSet;
+
+	// for test example i
+	for(size_t i = 1; i < testData.size(); i++){
+	vector<double> py;					// remake py for each loop
+	py.push_back(0);					// pad with a 0 -- REMOVE THIS IF CLASS LABEL STARTS AT ZERO
+	int selection = 0;			
+
+		// for each class j - SHOULD THIS START WITH 0 OR 1?
+		for(size_t j = 1; j < p_iy.size(); j++){
+			py.push_back(0);
+			//for each feature k
+			for(size_t k = 0; k < p_iy[i].first.size(); k++){
+				inSet = false;
+				size_t m;
+				// check if feature present
+				for(m = 0; m < testData[i].second.size(); m++){
+					if(testData[i].second[m].first == k){
+						inSet = true;
+						break;
+					}
+				}
+
+				// if feature present, add log(p)^(instances).  
+				if(inSet == true)
+					py[j] += pow(log(p_iy[j].first[k] / p_iy[j].second), testData[i].second[m].second);
+			}
+		}
+
+		// classify
+		double maxpy = -10000000000000; // maybe replace value for initial maxpy with a constant?  
+		for(size_t j = 1; j < p_iy.size(); j++){ 
+			if(py[j] > maxpy){
+				maxpy = py[j];
+				selection = j;
+			}
+		}
+		// add to classification vector
+		classification[i] = selection;
+	}
+	return classification;
+}
+
+void printToFile(string filename, vector<vector<int>> solution){
+	ofstream outStream (filename);
+
+	for(size_t i = 1; i < solution.size(); i++){
+		for(size_t j = 1; j < solution[i].size(); j++){
+			outStream << solution[i][j] << ",";
+		}
+		outStream << endl;
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	vector<string> labels;
@@ -81,7 +269,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//			aka a vector of int, int.
 	vector<pair<int, vector<pair<int, int>>>> testData;
 	// again, 1-indexing, so add a dummy
-	vector<pair<int, int>> dummy(0, make_pair(0, 0));
+	//vector<pair<int, int>> dummy(0, make_pair(0, 0));
 	testData.push_back(make_pair(0, dummy));
 	ifstream testDataFile("test.data");
 	ifstream testLabels("test.label");
@@ -127,188 +315,3 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
-// result of training: list of p_i|y and x_i for all i elements of vocabulary
-// p_i|y needs Laplace smoothing, aka add 1 to numerator and 2 to denominator
-// then to test: calculate p(x|y), straightforward enough
-/*
-	to calculate a single p(x|y)
-		p = 0
-		for all words i
-			n1 = num(class y examples where word i appears) + 1
-			n = num(class y examples) + 2
-			//p_i|y = n1/n
-			// sum of log(a)s rather than product of as
-			if i present in x
-				p += log(n1/n)
-			else
-				p += log(1 - n1/n)
-
-	so to train all
-	vector of y vectors of length i holding p_i|y
-	use above method but instead of p +=, just assign p_vec[y][i] = p_i|y
-	
-*/
-vector<pair<vector<int>, int>> naiveBayesBernoulliTraining(vector<string> labels, vector<string> dictionary, vector<pair<int, vector<pair<int, int>>>> trainingData){
-	vector<int> appearances(dictionary.size(), 1); // laplace smoothing - 1 in numerator
-	vector<pair<vector<int>, int>> p_iy(labels.size(), make_pair(appearances, 2)); // laplace smoothing - 2 in denominator
-
-	for(auto example : trainingData) {
-		int y = example.first;
-		p_iy[y].second++; // instance of class y seen
-		for(auto word : example.second) {
-			p_iy[y].first[word.first]++; // at least one appearance of word i in class y
-		}
-	}
-	return p_iy;
-}
-
-// needs test data as input -- make output
-vector<int> naiveBayesBernoulliTest(vector<pair<int, vector<pair<int, int>>>> testData, vector<pair<vector<int>, int>> p_iy) {
-	// iterative over test data
-	/*
-		for example x
-		py = 0f
-		for each possible class y
-			for each feature xi
-				if xi == 1
-					py += log( p_iy[y].first[i] / p_iy[y].second )
-				else
-					py += log ( 1 - above )
-		pick highest probability class
-	*/
-
-	vector<int> classification;			// compare to test label at some point
-	bool inSet;
-
-	// for test example i
-	for(int i = 1; i < testData.size(); i++){
-	vector<double> py;					// remake py for each loop
-	py.push_back(0);					// pad with a 0 -- REMOVE THIS IF CLASS LABEL STARTS AT ZERO
-
-		// for each class j - SHOULD THIS START WITH 0 OR 1?
-		for(int j = 1; j < p_iy.size(); j++){
-			py.push_back(0);
-			//for each feature k
-			for(int k = 0; k < p_iy[i].first.size(); k++){
-				inSet = false;
-
-				// check if feature present
-				for(int m = 0; m < testData[i].second.size(); m++){
-					if(testData[i].second[m].first == k){
-						inSet = true;
-						break;
-					}
-				}
-
-				// if feature present, add log(p).  Else, add log(1 - p)
-				if(inSet == true)
-					py[j] += log(p_iy[j].first[k] / p_iy[j].second);
-				else
-					py[j] += 1 - log(p_iy[j].first[k] / p_iy[j].second);
-
-			}
-		}
-
-		// classify
-		double maxpy = -10000000000000, selection = 0;			// maybe replace value for initial maxpy with a constant?  
-		for(int j = 1; j < p_iy.size(); j++){ 
-			if(py[j] > maxpy){
-				maxpy = py[j];
-				selection = j;
-			}
-		}
-		// add to classification vector
-		classification[i] = selection;
-	}
-
-	return classification;
-
-}
-
-vector<vector<int>> formSolutionMatrix(vector<int> classification, 	vector<pair<int, vector<pair<int, int>>>> testData, int numClasses){
-	
-	// create solution matrix
-	vector<vector<int>> solution;
-	solution.resize(numClasses);			// numClasses may actually be number of classes plus one w/ zero unused
-	for(int i = 0; i < solution.size(); i++){
-		solution[i].resize(numClasses, 0);
-	}
-
-	for(int i = 0; i < classification.size(); i++){
-		solution[testData[i].first][classification[i]]++;
-	}
-
-	return solution;
-}
-
-vector<pair<vector<int>, int>> naiveBayesMultinomialTraining(vector<string> labels, vector<string> dictionary, vector<pair<int, vector<pair<int, int>>>> trainingData){
-	vector<int> appearances(dictionary.size(), 1); // laplace smoothing - 1 in numerator
-	vector<pair<vector<int>, int>> p_iy(labels.size(), make_pair(appearances, dictionary.size())); // laplace smoothing - vocab size in denominator
-
-	for(auto example : trainingData) {
-		int y = example.first;
-		for(auto word : example.second) {
-			// count all words in group/class y
-			p_iy[y].second += word.second;
-			// count of word i in class y examples
-			p_iy[y].first[word.first] += word.second;
-		}
-	}
-	return p_iy;
-}
-
-vector<int> naiveBayesMultinomialTest(vector<pair<int, vector<pair<int, int>>>> testData, vector<pair<vector<int>, int>> p_iy) {
-	// this algorithm might be exactly the same as for the Bernoulli test. we may not need both functions.
-		vector<int> classification;			// compare to test label at some point
-	bool inSet;
-
-	// for test example i
-	for(int i = 1; i < testData.size(); i++){
-	vector<double> py;					// remake py for each loop
-	py.push_back(0);					// pad with a 0 -- REMOVE THIS IF CLASS LABEL STARTS AT ZERO
-
-		// for each class j - SHOULD THIS START WITH 0 OR 1?
-		for(int j = 1; j < p_iy.size(); j++){
-			py.push_back(0);
-			//for each feature k
-			for(int k = 0; k < p_iy[i].first.size(); k++){
-				inSet = false;
-				int m;
-				// check if feature present
-				for(m = 0; m < testData[i].second.size(); m++){
-					if(testData[i].second[m].first == k){
-						inSet = true;
-						break;
-					}
-				}
-
-				// if feature present, add log(p)^(instances).  
-				if(inSet == true)
-					py[j] += pow(log(p_iy[j].first[k] / p_iy[j].second), testData[i].second[m].second);
-			}
-		}
-
-		// classify
-		double maxpy = -10000000000000, selection = 0;			// maybe replace value for initial maxpy with a constant?  
-		for(int j = 1; j < p_iy.size(); j++){ 
-			if(py[j] > maxpy){
-				maxpy = py[j];
-				selection = j;
-			}
-		}
-		// add to classification vector
-		classification[i] = selection;
-	}
-	return classification;
-}
-
-void printToFile(string filename, vector<vector<int>> solution){
-	ofstream outStream (filename);
-
-	for(int i = 1; i < solution.size(); i++){
-		for(int j = 1; j < solution[i].size(); j++){
-			outStream << solution[i][j] << ",";
-		}
-		outStream << endl;
-	}
-}
